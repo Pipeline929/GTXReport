f931d9634a98e62451c5432321cde66397cc2696
Pipeline929 <75398443+Pipeline929@users.noreply.github.com>
2020-12-09 18:55:22 -0600

    updated end shift button to alert and log in console end shift time and date

--- GTXButtonCode.js
+++ GTXButtonCode.js
@@ -15,20 +15,11 @@ function report() {
 }
 //Shift End Button
 function shiftend() {
-<<<<<<< HEAD
     //displays alert of shift end.
     alert('Shift Ended');
     //console prints date and time of shift end
     alert('Shift ended on ' + month + '/' + day + '/' + year + ' at ' + hour + ':' + minute + ':' + second);
     console.log('Shift ended on ' + month + '/' + day + '/' + year + ' at ' + hour + ':' + minute + ':' + second);
-=======
-    var start = confirm('End Shift?');
-    if (start == true) {
-        alert('Shift ended on ' + month + '/' + day + '/' + year + ' at ' + hour + ':' + minute + ':' + second);
-        //console prints date and time of shift start
-        console.log('Shift ended on ' + month + '/' + day + '/' + year + ' at ' + hour + ':' + minute + ':' + second);
-    }
->>>>>>> 668819ba404f72a1e12e97ac652ae484c103aa13
 }
 //testing report data button
 function GTX() {
